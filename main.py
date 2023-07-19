c7653584516487e8bec73fa60bf5d4c4ef8278d6
freemanmaxwell <freeman.maxwell@outlook.com>
2023-07-19 12:29:14 -0500

    fixed metric array generation bug

--- main.py
+++ main.py
@@ -3,22 +3,14 @@ import scipy as sc
 import scipy.interpolate
 import matplotlib.pyplot as plt
 import math
-from shapely.geometry import Polygon
 import pygmsh
-import pyvista as pv
-import dolfinx.plot as plot
 import dolfinx
 from mpi4py import MPI
-import meshio
-import sys
-import gmsh
-from dolfinx.io import XDMFFile, gmshio
-from mpi4py import MPI
-from pathlib import Path
 import ufl
 
 def in2m(x):
-    return 0.0254*x
+    x_np = np.asarray(x)
+    return 0.0254 * x_np
 
 flexure = { # all units in m
     'total_length': in2m(2.5),
@@ -40,7 +32,7 @@ def generate_flexure_mesh(pts, mesh_density=0.01):
 
     def generate_flexure_function(pts, plot=False):
         flexure_length = flexure['total_length'] - flexure['foot_length'] - flexure['center_length'] - flexure['center_buffer_length']
-
+        
         # Create the flexure from the chosen parameters
         dx = flexure_length / (len(pts) + 1)
         x_pts_flexure = np.append([flexure['foot_length']], np.linspace(flexure['foot_length'] + dx, flexure['foot_length'] + flexure_length, len(pts)))
@@ -114,6 +106,8 @@ def generate_flexure_mesh(pts, mesh_density=0.01):
     top_edge = np.transpose([x3, y3])[::-1]
     contour = np.concatenate((bottom_edge, top_edge))
     
+    print(contour)
+
     with pygmsh.geo.Geometry() as geom:
         geom.add_polygon(
             contour,
@@ -136,8 +130,8 @@ def display_mesh(mesh):
     vertices = np.array(mesh.points)
     cells = np.array(mesh.cells_dict["triangle"])  # Assuming you have triangles
 
-    # Extract the x and y coordinates of the vertices
-    x, y = vertices[:, 0], vertices[:, 1]
+    x = vertices[:, 0]
+    y = vertices[:, 1]
 
     # Plot the mesh using matplotlib
     plt.figure(figsize=(8, 6))
@@ -148,27 +142,9 @@ def display_mesh(mesh):
     plt.gca().set_aspect("equal")
     plt.show()
 
-class fem:
-    def __init__(self):
-        self.flag = 0
-    def define_function_space():
-        # Define the second-order Lagrange (quadratic) element
-        element = ufl.VectorElement("Lagrange", mesh.ufl_cell(), 2)
 
-        # Create the vector-valued function space
-        V = dolfinx.FunctionSpace(mesh, element)
+pts = in2m([0.1, -0.2, 0.3, -0.4, 0.5])
+mesh, pymesh = generate_flexure_mesh(pts, mesh_density=in2m(0.01))
 
-        # Define the test and trial functions on the function space
-        u = ufl.TrialFunction(V)
-        v = ufl.TestFunction(V)
-
-def fem_setup(file):
-    xdmf_file = dolfinx.io.XDMFFile(MPI.COMM_WORLD, 'meshes/mesh.xdmf', "r")
-    mesh = xdmf_file.read_mesh(name="Grid")
-    mesh.topology.create_connectivity_all()
-
-
-pts = [0.1, -0.2, 0.3, -0.4, 0.5]
-mesh, pymesh = generate_flexure_mesh(pts)
 
 

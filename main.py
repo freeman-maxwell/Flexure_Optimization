d42ec7f2b1ac8dac0df6b7b4cbcaead96d4bd0a4
freemanmaxwell <freeman.maxwell@outlook.com>
2023-07-19 12:04:29 -0500

    generate_flexure_mesh outputs dolfinx mesh for FEM

--- main.py
+++ main.py
@@ -14,22 +14,36 @@ import sys
 import gmsh
 from dolfinx.io import XDMFFile, gmshio
 from mpi4py import MPI
+from pathlib import Path
+import ufl
 
-total_length = 2.5
-foot_length = 0.37
-center_length = 0.3125
-center_buffer_length = 0.15
-thickness = 1 / 40
+def in2m(x):
+    return 0.0254*x
+
+flexure = { # all units in m
+    'total_length': in2m(2.5),
+    'foot_length': in2m(0.37),
+    'center_length': in2m(0.3125),
+    'center_buffer_length': in2m(0.15),
+    'thickness': in2m(1 / 40)
+}
+
+material = {
+    # Aluminum H32-5052
+    "E": 70.3E9, # Young's Modulus, in Pa
+    "v": 0.33, # Poisson's Ratio
+    "Yield": 193E6 #Yield Strength, in Pa
+}
 
 
-def generate_flexure_contour(pts):
+def generate_flexure_mesh(pts, mesh_density=0.01):
 
     def generate_flexure_function(pts, plot=False):
-        flexure_length = total_length - foot_length - center_length - center_buffer_length
+        flexure_length = flexure['total_length'] - flexure['foot_length'] - flexure['center_length'] - flexure['center_buffer_length']
 
         # Create the flexure from the chosen parameters
         dx = flexure_length / (len(pts) + 1)
-        x_pts_flexure = np.append([foot_length], np.linspace(foot_length + dx, foot_length + flexure_length, len(pts)))
+        x_pts_flexure = np.append([flexure['foot_length']], np.linspace(flexure['foot_length'] + dx, flexure['foot_length'] + flexure_length, len(pts)))
         y_pts_flexure = np.append([0], pts)
 
         spline = sc.interpolate.CubicSpline(x_pts_flexure, y_pts_flexure, bc_type='clamped')
@@ -37,16 +51,16 @@ def generate_flexure_contour(pts):
         def flexure_function(x_data):
             y = []
             for x in x_data:
-                if 0 <= x <= foot_length:
+                if 0 <= x <= flexure['foot_length']:
                     y.append(0)
-                elif foot_length < x < foot_length + flexure_length:
+                elif flexure['foot_length'] < x < flexure['foot_length'] + flexure_length:
                     y.append(spline(x))
-                elif foot_length + flexure_length <= x:
+                elif flexure['foot_length'] + flexure_length <= x:
                     y.append(pts[-1])
             return np.array(y)
 
         if plot:
-            x = np.linspace(0, total_length, 100)
+            x = np.linspace(0, flexure['total_length'], 100)
             y = flexure_function(x)
             plt.plot(x, y)
             plt.title('Flexure Shape')
@@ -73,7 +87,7 @@ def generate_flexure_contour(pts):
 
     def remove_loops(x_data, y_data, N=200):
         y_new = []
-        x_new = np.linspace(0, total_length, N)
+        x_new = np.linspace(0, flexure['total_length'], N)
         for index, x in enumerate(x_new):
             point_indices = []
             for i in range(len(x_data)-1):
@@ -91,33 +105,31 @@ def generate_flexure_contour(pts):
 
     func = generate_flexure_function(pts, plot=False)
 
-    x1 = np.array(np.linspace(0, total_length, 200))
+    x1 = np.array(np.linspace(0, flexure['total_length'], 200))
     y1 = func(x1)
-    x2, y2 = get_normals(x1, y1, thickness)
+    x2, y2 = get_normals(x1, y1, flexure['thickness'])
     x3, y3 = remove_loops(x2, y2, 200)
 
     bottom_edge = np.transpose([x1, y1])
     top_edge = np.transpose([x3, y3])[::-1]
     contour = np.concatenate((bottom_edge, top_edge))
     
-    '''with pygmsh.geo.Geometry() as geom:
+    with pygmsh.geo.Geometry() as geom:
         geom.add_polygon(
             contour,
-            mesh_size=0.01,
+            mesh_size=mesh_density,
         )
-        mesh = geom.generate_mesh()
+        pymesh = geom.generate_mesh()
 
-    meshio.write_points_cells('meshes/mesh.vtk',
-                                      mesh.points,
-                                      mesh.cells,
-                                      point_data=mesh.point_data,
-                                      cell_data=mesh.cell_data,
-                                      field_data=mesh.field_data,
-                                      file_format='vtk')'''
-    
+    # Extract the vertices and cells from the pygmsh mesh
+    vertices = np.array(pymesh.points)[:,:2]
+    cells = np.array(pymesh.cells_dict["triangle"])  # Assuming you have triangles
 
+    ufl_tri = ufl.Mesh(ufl.VectorElement("Lagrange", ufl.triangle, 1))
 
-    return mesh
+    mesh = dolfinx.mesh.create_mesh(MPI.COMM_WORLD, cells, vertices, ufl_tri)
+
+    return mesh, pymesh
 
 def display_mesh(mesh):
     # Extract the vertices and cells from the pygmsh mesh
@@ -136,19 +148,27 @@ def display_mesh(mesh):
     plt.gca().set_aspect("equal")
     plt.show()
 
+class fem:
+    def __init__(self):
+        self.flag = 0
+    def define_function_space():
+        # Define the second-order Lagrange (quadratic) element
+        element = ufl.VectorElement("Lagrange", mesh.ufl_cell(), 2)
+
+        # Create the vector-valued function space
+        V = dolfinx.FunctionSpace(mesh, element)
+
+        # Define the test and trial functions on the function space
+        u = ufl.TrialFunction(V)
+        v = ufl.TestFunction(V)
+
+def fem_setup(file):
+    xdmf_file = dolfinx.io.XDMFFile(MPI.COMM_WORLD, 'meshes/mesh.xdmf', "r")
+    mesh = xdmf_file.read_mesh(name="Grid")
+    mesh.topology.create_connectivity_all()
+
 
 pts = [0.1, -0.2, 0.3, -0.4, 0.5]
-meshV = generate_flexure_contour(pts)
-display_mesh(meshV)
-
-
-vtk_reader = dolfinx.io.VTKFile(MPI.COMM_WORLD, 'meshes/mesh.vtk', "r")
-
-# Read the mesh from the VTK file
-mesh = vtk_reader.
-
-
-mesh = dolfinx.io.VTKFile
-
+mesh, pymesh = generate_flexure_mesh(pts)
 
 
